95d6c0b759ea0e3f4ca60fe1c362c295f21ad47d
olayodeattipoe <mensahattipoesolomonolayode@gmail.com>
2024-12-24 15:40:54 +0000

    vit

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || 'http://192.168.51.163:8000',
+  BASE_URL: import.meta.env.VITE_API_URL || 'https://calabash-management-centre-95kaw.ondigitalocean.app',
   VITE_HOST: import.meta.env.VITE_HOST_IP || '192.168.51.163',
   VITE_PORT: import.meta.env.VITE_PORT || 5178
 }
