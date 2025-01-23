74513b798cd9baa2ee0b0ace13c27556ef39176d
olayodeattipoe <mensahattipoesolomonolayode@gmail.com>
2025-01-23 09:23:15 +0000

    changed url to bels

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || 'https://calabash-management-centre-95kaw.ondigitalocean.app',
+  BASE_URL: import.meta.env.VITE_API_URL || 'https://belsmanagementcentre-r33to.ondigitalocean.app',
   //BASE_URL: import.meta.env.VITE_API_URL || 'http://10.40.32.242:8000',
   VITE_HOST: import.meta.env.VITE_HOST_IP || '192.168.43.175',
   VITE_PORT: import.meta.env.VITE_PORT || 5178
