babdaf6d7c298c867da10d597b7f146ca5b76b7c
olayodeattipoe <mensahattipoesolomonolayode@gmail.com>
2025-03-14 07:44:26 +0000

    foul

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,14 @@
-//export const API_CONFIG = {
-  //BASE_URL: import.meta.env.VITE_API_URL || 'https://calabash-management-centre-95kaw.ondigitalocean.app',
-  //BASE_URL: import.meta.env.VITE_API_URL || 'http://10.40.32.242:8000',
-  //VITE_HOST: import.meta.env.VITE_HOST_IP || '192.168.43.175',
-  //VITE_PORT: import.meta.env.VITE_PORT || 5178
-//}
-
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || 'http://164.90.185.5:8000',
-  VITE_HOST: import.meta.env.VITE_HOST_IP || 'http://164.90.185.5',
+  BASE_URL: import.meta.env.VITE_API_URL || 'https://calabash-management-centre-95kaw.ondigitalocean.app',
+  BASE_URL: import.meta.env.VITE_API_URL || 'http://10.40.32.242:8000',
+  VITE_HOST: import.meta.env.VITE_HOST_IP || '192.168.43.175',
   VITE_PORT: import.meta.env.VITE_PORT || 5178
 }
+
+//export const API_CONFIG = {
+  //BASE_URL: import.meta.env.VITE_API_URL || 'http://164.90.185.5',
+  //BASE_URL: import.meta.env.VITE_API_URL || 'http://localhost'
+  //BASE_URL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  //VITE_HOST: import.meta.env.VITE_HOST_IP || 'localhost',
+  //VITE_PORT: import.meta.env.VITE_PORT || 5178
+//}
